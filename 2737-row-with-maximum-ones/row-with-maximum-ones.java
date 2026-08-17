5ba44c5b96c1958f5b83a3997a7fa088b73489c6
Akash Tripathi <akashtripathi9755@gmail.com>
2026-08-17 06:34:13 +0530

    Time: 10 ms (7.14%) | Memory: 48.8 MB (5.76%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.java
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.java
@@ -1,14 +1,29 @@
 class Solution {
+    public int lowerbound(int[] nums,int n, int x){
+        int low = 0;
+        int high = n-1;
+        int ans = n;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]>=x){
+                ans = mid;
+                high = mid-1;
+            }
+            else {
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
     public int[] rowAndMaximumOnes(int[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
         int cmt_max =0;
         int index = 0;
         for(int i=0; i<n; i++){
-            int cmt_ones = 0;
-            for(int j=0; j<m; j++){
-                cmt_ones += mat[i][j];
-            }
+            int [] row = mat[i].clone();
+            Arrays.sort(row);
+            int cmt_ones = m- lowerbound(row,m,1);
                 if(cmt_ones>cmt_max){
                     cmt_max = cmt_ones;
                     index = i;
